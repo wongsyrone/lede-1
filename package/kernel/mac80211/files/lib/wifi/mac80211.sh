e67a1b62405ac29a31a7d48a85851f35bfdba686
Rafał Miłecki <zajec5@gmail.com>
2016-05-24 09:33:25 +0200

    mac80211: change default SSID from Lede to LEDE
    LEDE project seems to be using "LEDE" as its acronym everywhere. To keep
    things consistent adjust default wireless SSID.
    
    Signed-off-by: Rafał Miłecki <zajec5@gmail.com>
    Acked-by: Jo-Philipp Wich <jo@mein.io>
    Acked-by: John Crispin <john@phrozen.org>
    Acked-by: Alexander Couzens <lynxis@fe80.eu>

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -130,7 +130,7 @@ config wifi-iface
 	option device   radio$devidx
 	option network  lan
 	option mode     ap
-	option ssid     Lede${ssid_5ghz}-$(cat /sys/class/ieee80211/${dev}/macaddress | awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)
+	option ssid     LEDE${ssid_5ghz}-$(cat /sys/class/ieee80211/${dev}/macaddress | awk -F ":" '{print $4""$5""$6 }'| tr a-z A-Z)
 	option encryption none
 
 EOF
