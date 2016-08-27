8b5cef8c9edb1169a8489bf9632b43022a311e88
Syrone Wong <wong.syrone@gmail.com>
2016-08-27 09:18:19 +0800

    download.mk: enlarge git clone depth

--- include/download.mk
+++ include/download.mk
@@ -92,7 +92,7 @@ define DownloadMethod/git
 		cd $(TMP_DIR)/dl && \
 		rm -rf $(SUBDIR) && \
 		[ \! -d $(SUBDIR) ] && \
-		git clone --depth 5 $(URL) $(SUBDIR) && \
+		git clone --depth 50 $(URL) $(SUBDIR) && \
 		(cd $(SUBDIR) && git checkout $(VERSION) && \
 		git submodule update --init --recursive) && \
 		echo "Packing checkout..." && \
