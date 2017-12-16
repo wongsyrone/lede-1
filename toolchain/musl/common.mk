5bf138d8ff25ce79c19ca348db5d50e3413576ab
Syrone Wong <wong.syrone@gmail.com>
2017-12-17 07:25:24 +0800

    toolchain/musl: update to latest; enable optimization

--- toolchain/musl/common.mk
+++ toolchain/musl/common.mk
@@ -9,12 +9,12 @@ include $(INCLUDE_DIR)/target.mk
 
 PKG_NAME:=musl
 PKG_VERSION:=1.1.18
-PKG_RELEASE=1
+PKG_RELEASE=2
 
 PKG_SOURCE_PROTO:=git
 PKG_SOURCE_SUBDIR:=$(PKG_NAME)-$(PKG_VERSION)
-PKG_SOURCE_VERSION:=72656157f54c47277b01ec85a6ba7c4084fea6c8
-PKG_MIRROR_HASH:=a3d857c23c94aa96a4ad5f442aaf236e5a189a717273c4e4faf425988d98cd32
+PKG_SOURCE_VERSION:=d5029bb88a4ff91b007a19c7d9efb790aab63246
+PKG_MIRROR_HASH:=
 PKG_SOURCE_URL:=git://git.musl-libc.org/musl
 PKG_SOURCE:=$(PKG_NAME)-$(PKG_VERSION)-$(PKG_SOURCE_VERSION).tar.xz
 
@@ -26,6 +26,8 @@ HOST_BUILD_DIR:=$(BUILD_DIR_TOOLCHAIN)/$(PKG_NAME)-$(PKG_VERSION)
 include $(INCLUDE_DIR)/toolchain-build.mk
 include $(INCLUDE_DIR)/hardening.mk
 
+TARGET_CFLAGS := $(filter-out -O%,$(TARGET_CFLAGS))
+
 MUSL_CONFIGURE:= \
 	$(TARGET_CONFIGURE_OPTS) \
 	CFLAGS="$(TARGET_CFLAGS)" \
@@ -35,7 +37,8 @@ MUSL_CONFIGURE:= \
 		--host=$(GNU_HOST_NAME) \
 		--target=$(REAL_GNU_TARGET_NAME) \
 		--disable-gcc-wrapper \
-		--enable-debug
+		--enable-debug \
+		--enable-optimize
 
 define Host/Prepare
 	$(call Host/Prepare/Default)
