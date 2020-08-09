cb714f9fc810d185f347f10f7d7ac5aaef64eadd
Syrone Wong <wong.syrone@gmail.com>
2020-08-09 23:34:30 +0800

    x86: 64: remove default package of e1000e and igb
    in favor of the Intel -oot packages
    
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -1,6 +1,6 @@
 define Device/generic
   DEVICE_TITLE := Generic x86/64
-  DEVICE_PACKAGES += kmod-bnx2 kmod-e1000e kmod-e1000 kmod-forcedeth kmod-igb \
+  DEVICE_PACKAGES += kmod-bnx2 kmod-e1000 kmod-forcedeth \
 	kmod-ixgbe kmod-r8169
   GRUB2_VARIANT := generic
 endef
