b854cce595bff474fe75954629a18ead4cf23096
Syrone Wong <wong.syrone@gmail.com>
2020-12-07 21:40:59 +0800

    import upstream shortcut-fe fixes
    From b66d335c8369843dec4f3e92a21dbe40b1a51bed Mon Sep 17 00:00:00 2001
    From: quarkysg <hhtan72@yahoo.com>
    Date: Thu, 25 Jun 2020 14:49:37 +0800
    Subject: [PATCH] shortcut-fe: rework netfilter conntrack notification
    
    The original patch over rode the nf_conntrack_un/register_notifier API, which will
    break other modules relying on the API. Reworked the notification APIs to play nice
    with others. Also avoid to touch the code of fullcone nat.
    
    https://patch-diff.githubusercontent.com/raw/coolsnowwolf/lede/pull/5760.patch
    
    Signed-off-by: Syrone Wong <wong.syrone@gmail.com>

--- package/external/sfe/shortcut-fe/src/sfe_cm.c
+++ package/external/sfe/shortcut-fe/src/sfe_cm.c
@@ -1049,14 +1049,14 @@ static int __init sfe_cm_init(void)
 	 */
 #ifdef CONFIG_NF_CONNTRACK_EVENTS
 #ifdef CONFIG_NF_CONNTRACK_CHAIN_EVENTS
-	(void)nf_conntrack_register_notifier(&init_net, &sfe_cm_conntrack_notifier);
+	result = nf_conntrack_register_chain_notifier(&init_net, &sfe_cm_conntrack_notifier);
 #else
 	result = nf_conntrack_register_notifier(&init_net, &sfe_cm_conntrack_notifier);
+#endif
 	if (result < 0) {
 		DEBUG_ERROR("can't register nf notifier hook: %d\n", result);
 		goto exit4;
 	}
-#endif
 #endif
 
 	spin_lock_init(&sc->lock);
@@ -1123,8 +1123,11 @@ static void __exit sfe_cm_exit(void)
 	sfe_ipv6_destroy_all_rules_for_dev(NULL);
 
 #ifdef CONFIG_NF_CONNTRACK_EVENTS
+#ifdef CONFIG_NF_CONNTRACK_CHAIN_EVENTS
+	nf_conntrack_unregister_chain_notifier(&init_net, &sfe_cm_conntrack_notifier);
+#else
 	nf_conntrack_unregister_notifier(&init_net, &sfe_cm_conntrack_notifier);
-
+#endif
 #endif
 	nf_unregister_net_hooks(&init_net, sfe_cm_ops_post_routing, ARRAY_SIZE(sfe_cm_ops_post_routing));
 
