333788e0ee7369363195598ed30d21b43760bc04
Syrone Wong <wong.syrone@gmail.com>
2023-10-09 22:55:46 +0800

    luci-app-turboacc: adapt to my brcmfullcone and nft_fullcone

--- package/external/sfe/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
+++ package/external/sfe/luci-app-turboacc/luasrc/model/cbi/turboacc.lua
@@ -18,25 +18,30 @@ sw_flow.description = translate("Software based offloading for routing/NAT")
 sw_flow:depends("sfe_flow", 0)
 end
 
-if luci.sys.call("cat /etc/openwrt_release | grep -q mt762") == 0 then
+if luci.sys.call("cat /etc/openwrt_release | grep -Eq 'filogic|mt762' ") == 0 then
 hw_flow = s:option(Flag, "hw_flow", translate("Hardware flow offloading"))
 hw_flow.default = 0
-hw_flow.description = translate("Requires hardware NAT support. Implemented at least for mt762x")
+hw_flow.description = translate("Requires hardware NAT support, implemented at least for mt762x")
 hw_flow:depends("sw_flow", 1)
 end
 
-if nixio.fs.access("/lib/modules/" .. kernel_version .. "/shortcut-fe-cm.ko") then
+if nixio.fs.access("/lib/modules/" .. kernel_version .. "/shortcut-fe-cm.ko")
+or nixio.fs.access("/lib/modules/" .. kernel_version .. "/fast-classifier.ko")
+then
 sfe_flow = s:option(Flag, "sfe_flow", translate("Shortcut-FE flow offloading"))
 sfe_flow.default = 0
 sfe_flow.description = translate("Shortcut-FE based offloading for routing/NAT")
 sfe_flow:depends("sw_flow", 0)
 end
 
--- nftables
-if nixio.fs.access("/lib/modules/" .. kernel_version .. "/nft_fullcone.ko") then
-fullcone_nat = s:option(Flag, "fullcone_nat", translate("FullCone NAT"))
+fullcone_nat = s:option(Flag, "fullcone_nat", translate("FullCone NAT Global Switch"))
 fullcone_nat.default = 0
 fullcone_nat.description = translate("Using FullCone NAT can improve gaming performance effectively")
-end 
+
+fullcone_nat_mode = s:option(ListValue, "fullcone_nat_mode", translate("FullCone NAT Mode"))
+fullcone_nat_mode.default = 0
+fullcone_nat_mode:value("0", translate("nft_fullcone originated from Chion82"))
+fullcone_nat_mode:value("1", translate("Broadcom ASUS Merlin fullconenat in masquerade"))
+fullcone_nat_mode.description = translate("Using FullCone NAT can improve gaming performance effectively")
 
 return m
